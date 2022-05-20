9e08423a831e879f8562c397ba445f4fc315f2d2
Luiz Alberto <luiz4142@gmail.com>
2022-05-19 23:40:06 -0300

    Criação da função createParamsValidator(). Tem como finalidade criar validadores das propriedades de um objeto

--- desafio-sharenergy/src/utils/queryParamsValidator.js
+++ desafio-sharenergy/src/utils/queryParamsValidator.js
@@ -17,6 +17,39 @@ function isValidObject(object) {
   return true;
 }
 
+function createParamsValidator(validations) {
+  if (!isValidObject(validations)) return;
+
+  const validationFns = {};
+
+  for (const validationFnKey in validations) {
+    const validationFn = validations[validationFnKey];
+
+    if (isFunction(validationFn)) {
+      validationFns[validationFnKey] = validationFn;
+    }
+  }
+
+  return (params) => {
+    if (!isValidObject(params)) return;
+
+    const validParams = {};
+
+    for (const param in params) {
+      const paramValue = params[param];
+
+      if (
+        param in validationFns &&
+        validationFns[param](paramValue, param, params)
+      ) {
+        validParams[param] = paramValue;
+      }
+    }
+
+    return validParams;
+  };
+}
+
 const validations = {
   title_contains: (value) => value != "",
   _limit: (value) => [10, 25, 50, 100].includes(parseInt(value)),
